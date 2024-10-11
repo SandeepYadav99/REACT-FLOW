aea7d933c569cb6fadffe11e52078516836cf776
SandeepYadav99 <sandeepyadav.7one@gmail.com>
2024-10-11 12:23:07 +0530

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,41 @@
+import { useState } from "react";
+import SelectFiled from "./Components/SelectFiled/SelectFiled";
+import DUMMY_NODES from "./DummyNode/DUMMY_NODES";
+import styles from "./Styles.module.css";
+
+const App = () => {
+   const [selectOptions, setSelectOptions] = useState<string[]>([]);
+   
+  const handleSelectChange = (event: React.ChangeEvent<HTMLSelectElement>) => {
+    const eventValue = event.target.value;
+    if(!eventValue) return;
+    if(!selectOptions.includes(eventValue)){
+      setSelectOptions([...selectOptions, eventValue]);
+    }
+
+  };
+console.log(selectOptions);
+  return (
+    <div className={styles.dropdown}> 
+      <div className="formFlex">
+        <div className="formGroup">
+          <SelectFiled
+            label={"Select an options "}
+            onChange={handleSelectChange}
+          >
+          
+            {DUMMY_NODES?.map((dum) => {
+              return (
+                <option value={dum.name} key={dum.name}>
+                  {dum.value}
+                </option>
+              );
+            })}
+          </SelectFiled>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default App;
---
+++ src/DummyNode/DUMMY_NODES.tsx
@@ -0,0 +1,13 @@
+const DUMMY_NODES: {
+  value: string;
+  name: string;
+}[] = [
+  {value:"Select an option", name:""},
+  { value: "Stripe", name: "stripe" },
+  { value: "Paypal", name: "paypal" },
+  { value: "Google Pay", name: "google-pay" },
+  { value: "Apple Pay", name: "apple_pay" },
+  { value: "Amazon Pay", name: "Amazon Pay" },
+];
+
+export default DUMMY_NODES;
